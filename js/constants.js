5bad611983f8327ceead45f43953ebbbe122ffed
Mauricio López Moreno <maurikpo013@gmail.com>
2026-09-22 23:05:16 -0300

    fix: color del obelisco en el mapa mas visible y distinto del jugador

--- js/constants.js
+++ js/constants.js
@@ -2,7 +2,7 @@
    GTA MERCALOCA - Constantes y Configuración General
    ========================================================================= */
 
-const VERSION = 'v21-85061e2';
+const VERSION = 'v25-0619202';
 
 const CREW = [
   {id:'p1', name:'EL SMOKE',  img:'img/p1.jpeg', crop:[0.42,0.11,0.25,0.29], shirt:'#3f9a4a', pants:'#232323'},
